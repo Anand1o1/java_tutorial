4e3b2c1eb725b27066511ade110074888a8ff295
anand <anandandsahoo@gmail.com>
2022-10-14 21:02:26 +0530

    introduction of bits manupulation

---
+++ bitmanupulation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class bitmanupulation {
+    public static void main(String args[]) {
+        //binary left sift operator (<<) formula to find out the value is 
+        //a<<b= a*2^b
+        //5<<2=5*2^2=20;
+        System.out.println(5<<2);
+
+        //binary rightshift operator(>>)formula to find out the value is
+        //a>>b=a/2^b
+        //6>>1=6/2^1=3
+        System.out.println(6>>1);
+        
+    }
+}
